27f47fcbe95fa4922a2a2e086a03f17d531cab86
BnDaudlinux <lawalsulaimon70@gmail.com>
2025-09-21 03:04:05 +0100

    included seed.py

--- alx_travel_app/listings/management/commands/seed.py
+++ alx_travel_app/listings/management/commands/seed.py
@@ -1,1 +1,14 @@
-print("hello world")
+import sqlite3 
+from faker import Faker 
+
+
+fake = Faker()
+
+print(fake.name())
+
+
+
+### note
+# my faker is not working yet, that my 
+# for this file to be empty when you are reviewing it
+#before the next milestone, it will be update
